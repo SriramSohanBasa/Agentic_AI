9a294474bc75441d3b281e5d53ccbcb4296c8d60
SriramSohanBasa <basa.s@northeastern.edu>
2025-01-23 16:37:01 -0500

    chore: Updated paths

--- Financial AI analyst/playground.py
+++ Financial AI analyst/playground.py
@@ -1,98 +1,40 @@
-import openai
 from phi.agent import Agent
-import phi.api
-from phi.model.openai import OpenAIChat
-from phi.tools.yfinance import YFinanceTools
-from phi.tools.duckduckgo import DuckDuckGo
-from dotenv import load_dotenv
 from phi.model.groq import Groq
-
-import os
-import phi
+from phi.tools.duckduckgo import DuckDuckGo
+from phi.tools.yfinance import YFinanceTools
+from phi.storage.agent.sqlite import SqlAgentStorage
 from phi.playground import Playground, serve_playground_app
-# Load environment variables from .env file
+
+from dotenv import load_dotenv
+
 load_dotenv()
 
-phi.api=os.getenv("PHI_API_KEY")
-
-## web search agent
-web_search_agent=Agent(
-    name="Web Search Agent",
-    role="Search the web for the information",
-    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
-    tools=[DuckDuckGo()],
-    instructions=["Alway include sources"],
-    show_tools_calls=True,
-    markdown=True,
-
+web_search_agent = Agent(
+    name = "Web Agent",
+    description = "This is the agent for searching content from the web",
+    model = Groq(id="llama-3.3-70b-versatile"),
+    tools  = [DuckDuckGo()],
+    instructions = ["Always include the sources"],
+    show_tool_calls = True,
+    markdown = True,
+    debug_mode=True
 )
 
-## Financial agent
-finance_agent=Agent(
-    name="Finance AI Agent",
-    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
-    tools=[
-        YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True,
-                      company_news=True),
-    ],
-    instructions=["Use tables to display the data"],
+#web_search_agent.print_response("What is the capital of Nepal?", stream=True)
+
+finance_agent = Agent(
+    name="Finance Agent",
+    description = "Your task is to find the finance information",
+    model = Groq(id="llama-3.3-70b-versatile"),
+    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, company_info=True, company_news=True)],
+    instructions=["Use tables to display data"],
     show_tool_calls=True,
     markdown=True,
-
+    debug_mode = True
 )
+#finance_agent.print_response("Summarize analyst recommendations for NVDA", stream=True)
 
-import openai
-from phi.agent import Agent
-import phi.api
-from phi.model.openai import OpenAIChat
-from phi.tools.yfinance import YFinanceTools
-from phi.tools.duckduckgo import DuckDuckGo
-from dotenv import load_dotenv
-from phi.model.groq import Groq
+app = Playground(agents=[finance_agent, web_search_agent]).get_app()
 
-import os
-import phi
-from phi.playground import Playground, serve_playground_app
-# Load environment variables from .env file
-load_dotenv()
-
-phi.api=os.getenv("PHI_API_KEY")
-
-## web search agent
-web_search_agent=Agent(
-    name="Web Search Agent",
-    role="Search the web for the information",
-    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
-    tools=[DuckDuckGo()],
-    instructions=["Alway include sources"],
-    show_tools_calls=True,
-    markdown=True,
-
-)
-
-## Financial agent
-finance_agent=Agent(
-    name="Finance AI Agent",
-    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
-    tools=[
-        YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True,
-                      company_news=True),
-    ],
-    instructions=["Use tables to display the data"],
-    show_tool_calls=True,
-    markdown=True,
-
-)
-
-multi_ai_agent=Agent(
-    name="Multi Agent",
-    team=[web_search_agent,finance_agent],
-    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
-    instructions=["Always include sources", "Use tables to display data"],
-    show_tool_calls=True,
-    markdown=True,
-)
-app=Playground(agents=[multi_ai_agent]).get_app()
-
-if __name__=="__main__":
-    serve_playground_app("playground:app",reload=True)
+if __name__ == "__main__":
+    serve_playground_app("playground:app", reload=True)
